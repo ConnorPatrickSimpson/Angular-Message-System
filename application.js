873087e261911efba6772e846dc507a3436d8a6b
Connor Simpson <ConnorPatrickSimpson@gmail.com>
2016-06-12 22:33:26 -0500

    added conversation selection service

--- application.js
+++ application.js
@@ -1,8 +1,39 @@
 
-angular.module('messageClient.controllers', []);
-angular.module('messageClient.services', []);
+var client = angular.module('messageClient', []);
 
-angular.module('messageClient', [
-    'messageClient.controllers',
-    'messageClient.services'
+client.controller('listCtrl', ['$scope', 'conversationService', 
+    function ($scope, conversationService) {
+        // assign arbitrary array of conversation objects
+        $scope.conversations = [
+            {conv_id: 11111},
+            {conv_id: 22222},
+            {conv_id: 33333}
+        ];
+        $scope.setConv = function(conv_set) {
+            conversationService.setConv(conv_set);
+			$scope.conv_display = conversationService.getConv();
+        };
+	}
 ]);
+	
+client.factory('conversationService',
+    function() {
+        var conversationService = {};
+		conversationService.conv_set = 'none';
+			
+        conversationService.setConv = function(conv_set) {
+            this.conv_set = conv_set;
+        };
+        conversationService.getConv = function() {
+            return this.conv_set;
+        };
+		
+		return conversationService;
+	}
+);
+
+client.factory('repositoryService'),
+    function() {
+		var repositoryService = {};
+	}
+);
