cee0d9afef2bb55f7c838ba8eb907c4009916581
Connor Simpson <Connor Simpson>
2016-06-18 01:41:23 -0500

    added conversation selection and message submission

--- application.js
+++ application.js
@@ -1,14 +1,15 @@
 
 var client = angular.module('messageClient', []);
+var currentUser = "User 1";
 
-client.controller('listCtrl', ['$scope', 'conversationService', 
-    function ($scope, conversationService) {
-        // assign arbitrary array of conversation objects
-        $scope.conversations = [
-            {conv_id: 11111},
-            {conv_id: 22222},
-            {conv_id: 33333}
-        ];
+client.controller('listCtrl', ['$scope', 'conversationService', 'listRepository',
+    function ($scope, conversationService, listRepository) {
+		listRepository.fetchList().then(
+		    // Success
+		    function(response) {
+				$scope.conversations = response.data;
+			}
+		);
         $scope.setConv = function(conv_set) {
             conversationService.setConv(conv_set);
 			$scope.conv_display = conversationService.getConv();
@@ -16,16 +17,32 @@ client.controller('listCtrl', ['$scope', 'conversationService',
 	}
 ]);
 
-client.controller('convCtrl', ['$scope, 'conversationService',
-    function ($scope, conversationService) {
-		
+client.controller('convCtrl', ['$scope', 'conversationService', 'conversationRepository',
+    function ($scope, conversationService, conversationRepository) {
+		var conv_display = conversationService.getConv();
+        $scope.$watch(
+		    function() {return conversationService.getConv()},
+		    function(newValue) {
+			    conversationRepository.fetchConv(newValue).then(
+		            //Success
+			        function(response) {
+				        $scope.messages = response.data;
+			        }
+		        );
+		    }
+		);
+		$scope.submitMess = function() {
+			if ($scope.messages !== "undefined") {
+			    conversationRepository.sendMess(conv_display, currentUser, $scope.messages, $scope.mess_submit);
+			}
+		};
 	}
 ]);
 	
 client.factory('conversationService',
     function() {
         var conversationService = {};
-		conversationService.conv_set = 'none';
+		conversationService.conv_set;
 			
         conversationService.setConv = function(conv_set) {
             this.conv_set = conv_set;
@@ -38,8 +55,45 @@ client.factory('conversationService',
 	}
 );
 
-client.factory('repositoryService'),
-    function() {
-		var repositoryService = {};
+
+client.factory('listRepository', ['$http',
+    function($http) {
+		var listRepository = {};
+		
+		listRepository.fetchList = function() {
+			//temporary code to access local JSON file
+			//used in lieu of backend REST API
+			return $http.get('json/list.json');
+		};
+		
+		return listRepository;
 	}
-);
+]);
+
+client.factory('conversationRepository', ['$http',
+    function($http) {
+		var conversationRepository = {};
+				
+		conversationRepository.fetchConv = function(conv_id) {
+			//temporary code to access local JSON file
+			//used in lieu of backend REST API
+			var dir = 'json/';
+			var url = dir.concat(conv_id, '.json');
+			return $http.get(url);
+		};
+		
+		conversationRepository.sendMess = function(conv_id, user, mess_list, mess_content) {
+			//temporary code to modify messages object in $scope
+			//used in lieu of backend REST API
+			
+			//This does not actually append to the local JSON object.
+			//Apparently I need to use jQuery to do that, but fuck that.
+			//This is for testing, so if I end up using .NET to create 
+			//a backend, this code will be extraneous.
+			var message = {"mess_id": 00, "mess_user": user, "mess_content": mess_content };
+			mess_list.push(message);
+		};
+		
+		return conversationRepository;
+	}
+]);
